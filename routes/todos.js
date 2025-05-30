a58b1975ed48cdfea5ce63127f6f53a7b69c2a51
Aakarsh Garg <Aakarshgargg1@gmail.com>
2025-05-30 18:24:12 +0530

    updatetodo

--- routes/todos.js
+++ routes/todos.js
@@ -3,8 +3,10 @@
 
  const{createTodo}=require("../controllers/createTodo");
  const{getTodo,getTodoById}=require("../controllers/getTodo");
+ const{updateTodo}=require("../controllers/updateTodo")
 
  router.post("/createTodo",createTodo);
  router.get("/getTodos",getTodo);
  router.get("/getTodos/:id",getTodoById);
+ router.put("/updateTodo/:id",updateTodo);
  module.exports=router;
